599a886b79c1c96213ad50ac480e41a10714bcc3
eddxw <eduardo.jemb@gmail.com>
2018-12-06 02:00:48 -0700

    paperjs circles exercise

--- assets/js/paperscript.js
+++ assets/js/paperscript.js
@@ -1,16 +1,7 @@
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'red';
-var start = new Point(0, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [500, -50]);
 
-var myCircle = new Path.Circle(new Point(100, 70), 10);
-myCircle.fillColor = 'black';
-
-var circle2 = new Path.Circle(new Point(50,50), 300);
-circle2.fillColor = "purple";
+for (var x = 0; x < 400; x += 25) {
+    for (var y = 0; y < 400; y += 25) {
+        var circle2 = new Path.Circle(new Point(x + 10, y + 10), 10);
+        circle2.fillColor = "black";
+    }
+}
